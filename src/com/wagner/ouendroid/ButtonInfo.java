c0d1b9c2f803c58a53ea45015b442191c8e0d71e
Alex Shulgach <shulga@rpi.edu>
2011-12-04 23:32:17 -0500

    comments

--- src/com/wagner/ouendroid/ButtonInfo.java
+++ src/com/wagner/ouendroid/ButtonInfo.java
@@ -1,11 +1,9 @@
 package com.wagner.ouendroid;
 
 /**
- * Created by IntelliJ IDEA.
  * User: Alex
  * Date: 12/3/11
  * Time: 2:58 PM
- * To change this template use File | Settings | File Templates.
  */
 public class ButtonInfo {
     int time;
--- src/com/wagner/ouendroid/FileReader.java
+++ src/com/wagner/ouendroid/FileReader.java
@@ -1,11 +1,9 @@
 package com.wagner.ouendroid;
 
 /**
- * Created by IntelliJ IDEA.
  * User: Alex
  * Date: 12/3/11
  * Time: 2:45 PM
- * To change this template use File | Settings | File Templates.
  */
 
 import java.io.File;
